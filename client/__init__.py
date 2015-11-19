da8926036c9c652b8202d8baf3e8277a357d8d92
Manuel Villarejo <mvillarejo@riplife.es>
2015-11-19 18:40:35 +0100

    - tests, exceptions, urls, log...  - get_nodes and get_node working ok  - login raises exceptions now

---
+++ client/__init__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+"""
+opennms-client
+~~~~~~~~~~~~~~~~~~~~~
+
+opennms-client is a simple library for managing Fifa Ultimate Team.
+Usage:
+    >>> import client
+    to be continued ;-)
+:copyright: (c) 2015 by Manuel Villarejo.
+:license: MIT.
+"""
+
+__title__ = 'opennms-client'
+__version__ = '0.1'
+__author__ = 'Manuel Villarejo'
+__author_email__ = 'mjvillarejo@gmail.com'
+__license__ = 'MIT License'
+__copyright__ = 'Copyright 2015 Manuel Villarejo'
+
+
+from .client import Client
