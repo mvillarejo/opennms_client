5e6b5a6c391132d707a8fb7defb01a78e6708e60
Manuel Villarejo <mvillarejo@riplife.es>
2015-11-19 13:58:21 +0100

    Initial Commit from python-package-skeleton + local modifications

---
+++ setup.py
@@ -0,0 +1,35 @@
+import os
+from setuptools import setup, find_packages
+
+from myapp import __version__
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+requirements = []
+
+setup(
+    name = "opennms-client",
+    version = ".".join(map(str, __version__)),
+    description = "",
+    long_description = read('README.rst'),
+    url = '',
+    license = 'MIT',
+    author = 'mvillarejo',
+    author_email = '',
+    packages = find_packages(exclude=['tests']),
+    include_package_data = True,
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        #'Framework :: Django',
+    ],
+    install_requires = requirements,
+    tests_require = [],
+)
