ee2f903cf0830ca086c9774d9590bf6cc4ed25e7
Manuel Villarejo <mvillarejo@riplife.es>
2015-11-20 12:40:38 +0100

    - setup.py tuned for releasing

--- client/__init__.py
+++ client/__init__.py
@@ -12,12 +12,4 @@ Usage:
 :license: MIT.
 """
 
-__title__ = 'opennms-client'
-__version__ = '0.1'
-__author__ = 'Manuel Villarejo'
-__author_email__ = 'mjvillarejo@gmail.com'
-__license__ = 'MIT License'
-__copyright__ = 'Copyright 2015 Manuel Villarejo'
-
-
 from .client import Client
--- setup.py
+++ setup.py
@@ -1,34 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 import os
 from setuptools import setup, find_packages
-from client import __version__, __author__, __author_email__
 
 
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
+__title__ = 'opennms-client'
+__version__ = '0.1'
+__author__ = 'Manuel Villarejo'
+__author_email__ = 'mjvillarejo@gmail.com'
+__license__ = 'MIT License'
+__copyright__ = 'Copyright 2015 Manuel Villarejo'
 
-requirements = []
+packages = [
+    __title__
+]
+
+with open('requirements.txt') as f:
+    requires = f.read().splitlines()
+
 
 setup(
     name="opennms-client",
     version= __version__,
     description="",
     long_description=read('README.rst'),
-    url='',
-    license='MIT',
+    url='https://github.com/mvillarejo/%s' % __title__,
+    license=__license__,
     author=__author__,
     author_email=__author_email__,
+    download_url='https://github.com/mvillarejo/%s/releases' % __title__,
+    bugtrack_url='https://github.com/mvillarejo/%s/issues' % __title__,
+    platforms='any',
+    keywords='%s opennms client python' % __title__,
+    package_data={'': ['LICENSE']},
+    package_dir={__title__: __title__},
     packages=find_packages(exclude=['tests']),
     include_package_data=True,
-    classifiers=[
-        'Development Status :: 3 - Alpha',
+    install_requires=requires,
+    tests_require=[],
+        classifiers=[
+        'Development Status :: 4 - Beta',
         'Environment :: Console',
         'Intended Audience :: Developers',
+        'Natural Language :: English',
         'Intended Audience :: Information Technology',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
-        'Programming Language :: Python',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Software Development :: Libraries :: Python Modules',
     ],
-    install_requires=requirements,
-    tests_require=[],
 )
