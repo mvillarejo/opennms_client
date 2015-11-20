9c2751a6937fe076d41bd82dee65135dd3fdf6f3
Manuel Villarejo <mvillarejo@riplife.es>
2015-11-20 15:52:50 +0100

    - bumped version to 0.1.1

--- CHANGELOG.rst
+++ CHANGELOG.rst
@@ -7,3 +7,7 @@ Changelog
 0.1 (2015-11-20)
 ++++++++++++++++
 * client: version 0.1
+
+0.1.1 (2015-11-20)
+++++++++++++++++
+* client: added set_service(hostname, service_name)
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except ImportError:
     from distutils.core import setup
 
 __title__ = 'opennms-client'
-__version__ = '0.1'
+__version__ = '0.1.1'
 __author__ = 'Manuel Villarejo'
 __author_email__ = 'mjvillarejo@gmail.com'
 __license__ = 'MIT License'
